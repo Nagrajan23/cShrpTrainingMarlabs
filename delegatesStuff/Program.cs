b8d4dfb0173b80c2d18b00602030bd40ffa85545
Nagrajan23 <c.raj.23@gmail.com>
2018-02-01 15:20:41 -0500

    Add project files.

---
+++ delegatesStuff/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using factorialLibrary;
+using System.Threading;
+
+namespace delegatesStuff
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Small Factorial Calculation
+            /*Factorial obj = new Factorial();
+            BigInteger result = obj.Calculate(5);
+            Console.WriteLine(result); //*/
+
+            //Big Number Calculation - Synchronous call
+            /*Console.WriteLine("Started Calculation");
+            result = obj.Calculate(100000);
+            Console.WriteLine(result);
+            Console.WriteLine("Finished"); //*/
+
+            //Big Number Calculation - Asynchronous call, broadcast
+            Factorial obj = new Factorial();
+            obj.WhereToSend += DisplayFactorial;
+            obj.WhereToSend += DisplayFactorialGreater10k;
+            //obj.WhereToSend(100);
+            Console.WriteLine("Started Calculation");
+            Thread t1 = new Thread(() => obj.Calculate(100));
+            t1.Start();
+            Console.WriteLine("Finished");
+
+            Console.ReadKey();
+        }
+
+        static void DisplayFactorial(BigInteger result)
+        {
+            Console.WriteLine(result);
+        }
+
+        static void DisplayFactorialGreater10k(BigInteger result)
+        {
+            if(result > 10000)
+            {
+                Console.WriteLine("Greater Than 10k");
+            }
+            else
+            {
+                Console.WriteLine("Less Than 10k");
+            }
+        }
+    }
+}
---
+++ factorialLibrary/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace factorialLibrary
+{
+    public class Factorial
+    {
+        /*public BigInteger Calculate(BigInteger number)
+        {
+            BigInteger result = number;
+            for(int i = 1; i<number; i++)
+            {
+                result = result * i;
+            }
+            return result;
+        }//*/
+
+        public delegate void SendResult(BigInteger number);
+        //public SendResult WhereToSend;
+
+        //Events cannot publish data
+        public event SendResult WhereToSend;
+        public void Calculate(BigInteger number)
+        {
+            BigInteger result = number;
+            for (int i = 1; i < number; i++)
+            {
+                result = result * i;
+            }
+            WhereToSend(result);
+        }
+    }
+}
