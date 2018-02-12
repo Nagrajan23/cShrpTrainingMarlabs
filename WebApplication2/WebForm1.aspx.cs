7412dede70857d885b9c2cbc3aa78881867f9508
Nagrajan23 <c.raj.23@gmail.com>
2018-02-12 15:11:43 -0500

    WCF 3

---
+++ WebApplication2/WebForm1.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebApplication2
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            ServiceReference1.IService1 iservice = new ServiceReference1.Service1Client();
+            ServiceReference1.productData prod = new ServiceReference1.productData();
+            prod.ProductQuantity = 10;
+            prod.PerProductCost = 21;
+            prod.ProductName = "Apple";
+            Response.Write(iservice.getTotalCost(prod));
+        }
+    }
+}
